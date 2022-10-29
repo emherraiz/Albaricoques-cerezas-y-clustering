a14297e4400159dc833512fb30bd4f2ed00d8038
Enrique Marina <eherraiz@myuax.com>
2022-10-29 19:00:59 +0200

    nn

---
+++ aprendizaje_no_supervisado_bueno.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Clustering
+# Aprendizaje con el algoritmo K-Mean (K-Medias)
+# Para cantidades pequeñas de datos (inferior a 10.000)
+from sklearn.cluster import KMeans
