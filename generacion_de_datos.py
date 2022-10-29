b79c14560c32f48c3ef239eec661da0ae7d06fdd
Enrique Marina <eherraiz@myuax.com>
2022-10-29 18:10:12 +0200

    sd

--- generacion_de_datos.py
+++ generacion_de_datos.py
@@ -27,11 +27,18 @@ import pandas as pnd
 #---- CARACTERÍSTICAS------
 
 #CEREZAS
+# Diametro minimo (mm)
+# Diametro maximo (mm)
+# Peso minimo (g)
+# Peso maximo (g)
 caracteristicasCerezas = [[17,19,1,5],[20,21,5,6],[22,23,6,7],[24,25,7,8.5],[26,27,8.5,10],[28,29,10,11.5]]
 
 
 
 #ALBARICOQUES: ATENCIÓN DOS CASOS DE PRUEBAS EN FUNCIÓN DEL AVANCE DE SU LECTURA
+# Diametro minimo (mm)
+# Diametro maximo (mm)
+# Peso medio (g)
 
 #Caso 1:
 caracteristicasAlbaricoques = [[40,44,41],[45,49,54],[50,54,74],[55,59,100]]
@@ -48,25 +55,37 @@ cantidadObservaciones = 200
 cerezas = []
 random.seed()
 for iteration in range(cantidadObservaciones):
-    #elección al azar de una característica
+    # random.choice(): Elección al azar de una característica
     cereza = random.choice(caracteristicasCerezas)
-    #Generación de un diámetro
+
+    # Generación de un diámetro
+    # random.uniform(a, b): Valor aleatorio entre a y b
+    # Todos los valores son igual de probables dentro del intervalo
     diametro = round(random.uniform(cereza[0], cereza[1]),2)
-    #Generación de un peso
+
+    # Generación de un peso
     peso = round(random.uniform(cereza[2], cereza[3]),2)
-    print ("Cereza "+str(iteration)+" "+str(cereza)+" : "+str(diametro)+" - "+str(peso))
+    #print ("Cereza "+str(iteration)+" "+str(cereza)+" : "+str(diametro)+" - "+str(peso))
+
+    # Añadimos a la lista de cerezas el diametro y el peso de cada una de ellas
     cerezas.append([diametro,peso])
 
 
-#Generación de los albaricoques
+# Generación de los albaricoques
 albaricoques = []
 random.seed()
 for iteration in range(cantidadObservaciones):
-    #elección al azar de una característica
+    # Elección al azar de una característica
     albaricoque = random.choice(caracteristicasAlbaricoques)
-    #Generación de un diámetro
+
+    # Generación de un diámetro
     diametro = round(random.uniform(albaricoque[0], albaricoque[1]),2)
-    #Generación de un peso
+
+    # Generación de un peso
+    # El limite minimo y maximo de peso lo ponemos dividiendo y multiplicando por un valor respectivamente
+    # De esta manera el peso medio siempre va a ser el mismo
+    # Es aconsejable que este numero no sea muy grande para que los limites no difieran mucho de la realidad
+    # En este caso es un 10%
     limiteMinPeso = albaricoque[2] / 1.10
     limiteMaxPeso = albaricoque[2] * 1.10
     peso = round(random.uniform(limiteMinPeso, limiteMaxPeso),2)
@@ -75,6 +94,8 @@ for iteration in range(cantidadObservaciones):
 
 
 #Constitución de las observaciones
+# Concatenamos en una lista las observaciones de cerezas y albaricoques
+# De seguido mezclamos la lista al azar, de esta manera no sabremos donde se encuentra cada fruta
 frutas = cerezas+albaricoques
 print(frutas)
 
